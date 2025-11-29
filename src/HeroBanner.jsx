4188ae5a9aa81d05a7fb42ad25530a9e7d2cb4b3
Dewashish Mahto <dewashishm97@gmail.com>
2025-11-29 23:11:05 +0530

    Added fourth section(Hero Banner Section) of the website

---
+++ src/HeroBanner.jsx
@@ -0,0 +1,15 @@
+import section4Img from "./images/section4.jpg"
+import "./HeroBanner.css"
+
+export function HeroBanner() {
+    return(
+
+        <div className="herobanner">
+            
+            <img src={section4Img} className="sectionBanner" alt="" srcset="" />
+        </div>
+        
+    )
+
+
+}
