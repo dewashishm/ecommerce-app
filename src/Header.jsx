f7998e1e7a22ee687286927f0942d66df9fb8b5a
Dewashish Mahto <dewashishm97@gmail.com>
2025-12-04 23:58:43 +0530

    Updated images, edited search bar and other elements of header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,21 +1,39 @@
 import "./Header.css";
+import search from "./images/searches.png"
+import burgerbar from "./images/burger-bar.png"
+import cart from "./images/shopping-cart.png"
+import loginicon from "./images/person.png"
 
 
 export function Header() {
   return (
 
     <div>
-      <header id='navbar'>
+      <header id="navbar">
+        <div className="menu-container">
+        <img src={burgerbar} alt="" srcset="" className="menu"/>  
         <p>Amazon</p>
-        <div className='search-box'>
+
+        </div>
+
+
+
+
+
+
+        <div className='search-container'>
+
           <input type='text' placeholder="Search for Products..."></input>
-          <button id='btn'>Search</button>
+          <img src={search} alt="" srcset="" className="search-button" />
+
         </div>
 
         <div className="header-rightsection">
-          <p className="cart">Cart</p>
-          <p className="login">Login</p>
+          <img src={cart} alt="" srcset="" className="cart"/>
+          <img src={loginicon} alt="" srcset="" className="login"/>
+          
         </div>
+
 
       </header>
     </div>
