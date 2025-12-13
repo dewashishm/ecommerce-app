f0e85c1d5dc600f0c14e3d995e9367a9c0a85bc4
Dewashish Mahto <dewashishm97@gmail.com>
2025-12-13 23:59:05 +0530

    Added onClick functionallity and buttons

--- src/Slidemenu.jsx
+++ src/Slidemenu.jsx
@@ -1,6 +1,9 @@
-import "./Slidemenu.css"
-import { useState } from "react"
-import burgerbar from "./images/burger-bar.png"
+import "./Slidemenu.css";
+import { useState } from "react";
+import burgerbar from "./images/burger-bar.png";
+import close from "./images/close.png";
+
+
 
 export function Slidemenu() {
 
@@ -14,22 +17,19 @@ export function Slidemenu() {
 
 
             <img src={burgerbar} onClick={() => setIsOpen(true)} alt="" srcset="" className="menu" />
-            {isOpen && (
-                <>
-                <div className="sidebar">
+        
+                <div className={isOpen ? "sidebar open" : "sidebar"}>
+                    <img src={close} onClick={() => setIsOpen(false)} className="closebtn" />
+
                     <p className="menu-logo">Website Logo</p>
                     <p className="sidebar-elements">Home</p>
                     <p className="sidebar-elements">Products</p>
                     <p className="sidebar-elements">Contacts</p>
                     <p className="sidebar-elements">Others</p>
+
                 </div>
-                </>
-            )}
 
-
-            {/* <div className="slide-container">
-
-        </div> */}
+            
         </>
     )
 }
