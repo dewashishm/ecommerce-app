d5c7c9c0301ceb1adfe086afca3edcf64df93b8e
dewashishm <dewashishm97@gmail.com>
2025-12-23 23:54:56 +0530

    .

--- src/Components/Navigation Menu/Slidemenu.jsx
+++ src/Components/Navigation Menu/Slidemenu.jsx
@@ -2,6 +2,8 @@ import "./Slidemenu.css";
 import { useState } from "react";
 import burgerbar from "../../images/burger-bar.png";
 import close from "../../images/close.png";
+import { Link } from 'react-router-dom'
+import { Header } from "../Header/Header";
 
 
 
@@ -22,10 +24,10 @@ export function Slidemenu() {
                     <img src={close} onClick={() => setIsOpen(false)} className="closebtn" />
 
                     <p className="menu-logo">Website Logo</p>
-                    <p className="sidebar-elements">Home</p>
-                    <p className="sidebar-elements">Products</p>
-                    <p className="sidebar-elements">Contacts</p>
-                    <p className="sidebar-elements">Others</p>
+                    <Link to='/' onClick={() => setIsOpen(false)} className="sidebar-elements">Home</Link>
+                    <Link to='/products' onClick={() => setIsOpen(false)} className="sidebar-elements">Products</Link>
+                    <Link to='/contact' onClick={() => setIsOpen(false)} className="sidebar-elements">Contact</Link>
+                    <Link to='/about' onClick={() => setIsOpen(false)} className="sidebar-elements">About</Link>
 
                 </div>
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,15 @@
 import './Home.css';
-import { Header } from '../../Components/Header/Header.jsx';
 import { Banner } from '../../Components/Banner/Slider.jsx';
 import { FeaturedItems } from '../../Components/FeaturedItems/FeaturedItems.jsx';
 import { HeroBanner } from '../../Components/HeroBanner/HeroBanner.jsx'
-import { Footer } from '../../Components/Footer/Footer.jsx';
 
 export function Home() {
   return (
     <div className="App">
       
-      <Header/>  
       <Banner />
       <FeaturedItems />
       <HeroBanner />
-      <Footer />
 
 
     </div>
