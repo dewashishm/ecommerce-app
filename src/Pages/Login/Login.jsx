fd860db00e2e37557594021598a774ef120ff058
dewashishm <dewashishm97@gmail.com>
2026-01-09 23:56:09 +0530

    Added Login Page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,38 @@
+import './Login.css'
+
 
 function Login() {
-    return(
-        <div>Login page</div>
+    return (
+        <div className='login-page'>
+            
+            <div className='main-title'>
+            <p className='title'>Login</p>
+            <p className='title-h2'>Please fill your email and password to login</p>
+            </div>
+            
+            
+            
+            <div className='username'>
+            <p className='field-label'>Email Address</p>
+            <input type="text" className='input-box' placeholder='Type your Email...'/>
+            </div>
+            
+            
+            <div className='password'> 
+            <p className='field-label'>Password</p>
+            <input type="text" className='input-box' placeholder='*********'/>
+            </div>
+
+            <div className=''>
+                <button className='login-btn'>Log In</button>
+
+            </div>
+                <div>
+                <p>Don't have an account?</p>
+                <p>Register</p>
+                </div>
+    
+        </div>
     )
 }
 
