566d110aef5f913415e629f7dc2c8b4b5faac537
dewashishm <dewashishm97@gmail.com>
2026-01-10 23:53:23 +0530

    Updated classnames and some css properties

--- src/Pages/Login_and_Register/Login.jsx
+++ src/Pages/Login_and_Register/Login.jsx
@@ -1,37 +1,42 @@
 import './Login.css'
+import { Link } from 'react-router-dom'
+
 
 
 function Login() {
     return (
         <div className='login-page'>
-            
+
             <div className='main-title'>
-            <p className='title'>Login</p>
-            <p className='title-h2'>Please fill your email and password to login</p>
-            </div>
-            
-            
-            
-            <div className='username'>
-            <p className='field-label'>Email Address</p>
-            <input type="text" className='input-box' placeholder='Type your Email...'/>
-            </div>
-            
-            
-            <div className='password'> 
-            <p className='field-label'>Password</p>
-            <input type="text" className='input-box' placeholder='*********'/>
+                <p className='login-header'>Login</p>
+                <p className='login-subheader'>Please enter your email and password to login</p>
             </div>
 
-            <div className=''>
+
+
+            <div className='username'>
+                <p className='login-field-label'>Email Address</p>
+                <input type="text" className='login-input-box' placeholder='Type your Email...' />
+            </div>
+
+
+            <div className='password'>
+                <p className='login-field-label'>Password</p>
+                <input type="text" className='login-input-box' placeholder='*********' />
+            </div>
+
+            <div className='btn-div'>
                 <button className='login-btn'>Log In</button>
 
             </div>
-                <div>
+
+            <div className='login-footer'>
                 <p>Don't have an account?</p>
-                <p>Register</p>
-                </div>
-    
+                <Link to='/login/register'>
+                <p className='register-btn'>Register</p>
+                </Link>
+            </div>
+
         </div>
     )
 }
