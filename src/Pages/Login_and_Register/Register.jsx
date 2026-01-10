f3ea0eacac9ace0704e2b9d5dab0ae560618bd7f
dewashishm <dewashishm97@gmail.com>
2026-01-10 23:54:19 +0530

    Added Register component

---
+++ src/Pages/Login_and_Register/Register.jsx
@@ -0,0 +1,37 @@
+import './Register.css'
+
+function Register() {
+    return (
+        <div className='main-div'>
+            <p className='register-header'>Create an account</p>
+            <p className='register-subheader'>Create an account and start using....</p>
+
+            <div className='second-div'>
+                <p className='register-field-label'>Name</p>
+                <input type="text" placeholder='Enter your Name' className='register-input-box'/>
+                <p className='register-field-label'>Email Address</p>
+                <input type="text" placeholder='Enter your Email' name="" id="" className='register-input-box'/>
+                <p className='register-field-label'>Password</p>
+                <input type="text" name=""  placeholder='********' className='register-input-box'/>
+
+                <button className='btn-signup'>Sign Up</button>
+
+<div className='register-footer'>
+
+                <p>
+                    <span>I have read and agree to the</span>{' '} 
+                    <span className='footer-label'>Terms & Conditions</span>
+                </p>
+
+                <p>
+                    <span>Have an account?</span>{' '}
+                    <span className='footer-label'>Login</span>
+
+                </p>
+</div>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
