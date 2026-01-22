ef8ad5023e4a5c76c57ea0e2b645a592a19963f3
dewashishm <dewashishm97@gmail.com>
2026-01-22 23:34:22 +0530

    feat: add user model schema

---
+++ backend/models/user.js
@@ -0,0 +1,26 @@
+const { type } = require('@testing-library/user-event/dist/type');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase:true
+        },
+        password: {
+            type: String,
+            required: true
+
+        }
+    },
+    { timestamps: true}
+);
+
+module.exports = mongoose.model('User', userSchema);
