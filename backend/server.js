d87fd8347ac07438a872041f3e212678aa5fb439
dewashishm <dewashishm97@gmail.com>
2026-01-29 23:57:09 +0530

    Add CORS

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const { connect } = require('mongoose');
+// const { connect } = require('mongoose');
+const cors = require('cors');
 
 
 dotenv.config();
 connectDB();
-
 const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
+
 
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/auth', require("./routes/authRoutes"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
