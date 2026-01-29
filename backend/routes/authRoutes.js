6f0e106e578e198ab89ecaecbd56fd6b7ff7ba2c
dewashishm <dewashishm97@gmail.com>
2026-01-29 23:59:44 +0530

    .

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express')
 const {signup, login} = require('../controllers/authController');
 const {authMiddleware} = require('../middleware/authMiddleware');
 const User = require('../models/user');
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-router.post('/logout', logout)
+// router.post('/logout', logout)
 router.get('/user', authMiddleware, async(req, res)=> {
     const user = await User.findById(req.user.userId).select('-password');
 
