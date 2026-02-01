355228d7c2916efb239b4a55efed35f4a706a424
dewashishm <dewashishm97@gmail.com>
2026-02-01 23:57:30 +0530

    Add user profile support for authenticated users

--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,13 +1,46 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react';
 import "./Header.css";
 import search from "../../images/searches.png"
 // import burgerbar from "./images/burger-bar.png"
 import cart from "../../images/shopping-cart.png"
-
 import loginicon from "../../images/person.png"
 import Slidemenu from "../Navigation Menu/Slidemenu";
 
+
 function Header() {
+
+  const[user, setUser] = useState(null);
+  const [open, setOpen] = useState(false);
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const storedUser = localStorage.getItem("user");
+
+    if (token && storedUser) {
+      try{
+        setUser(JSON.parse(storedUser));
+
+
+      } catch (err) {
+        console.error("Invalid user data in local storage");
+        setUser(null);
+      }
+    }else{
+      setUser(null);
+    }
+   
+ }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setUser(null);
+    navigate("/login");
+  };
+
+
   return (
 
     <div>
@@ -21,19 +54,35 @@ function Header() {
         <div className='search-container'>
 
           <input type='text' placeholder="Search for Products..."></input>
-          <img src={search} alt="" srcset="" className="search-button" />
+          <img src={search} alt="" className="search-button" />
 
         </div>
 
         <div className="header-rightsection">
-          <img src={cart} alt="" srcset="" className="cart" />
-          <Link to='/login'>
-          <img src={loginicon} alt="" srcset="" className="login" />
-          </Link>
-          
+          {!user ? (
+            <Link to='/login' className='login-button'>
+              <img src={loginicon} alt="" className="login" /> <p>Login</p>
+            </Link>) : (
+              <div className="header-rightsection">
+              <span onMouseEnter={() => {setOpen(!open)}} onMouseLeave={() => {setOpen(open)}}>
+              <img src={loginicon} alt="" className="login" />
+                {user?.name} 
+              </span>
+
+              {open && (
+                <div className="dropdown">
+                  <Link to="/profile" onClick={() => setOpen(false)}>Profile</Link>
+                  <Link to="/settings" onClick={() => setOpen(false)}>Settings</Link>
+                  <Link to="/" onClick={logoutHandler}>
+                    Logout
+                  </Link>
+                </div>
+              )}
+            </div>
+          )}
+          <img src={cart} alt="" className="cart" />
 
         </div>
-
 
       </header>
     </div>
