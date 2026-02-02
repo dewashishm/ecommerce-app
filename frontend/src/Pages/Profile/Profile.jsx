736922f3aab18470758622d4b0a9bcc9dc742c7b
dewashishm <dewashishm97@gmail.com>
2026-02-02 23:58:28 +0530

    Update: State and Effect for user information

--- frontend/src/Pages/Profile/Profile.jsx
+++ frontend/src/Pages/Profile/Profile.jsx
@@ -1,16 +1,51 @@
+import { useEffect, useState } from "react";
 import "./Profile.css"
 
 function Profile () {
+
+    const [user, setUser] = useState(null);
+
+    useEffect(()=>{
+
+        const storedUser1 = localStorage.getItem("user");
+        
+        if (storedUser1) {
+            try{
+                setUser(JSON.parse(storedUser1));
+                // console.log(storedUser1);
+            }
+          catch (err){
+            console.log("Invalid user data");
+            setUser(null);
+          }
+        }
+          else{
+            setUser(null);
+          }
+    }, []);
+
+    
     return(
 
         <div className="profile">
+        <div className="profile-2">
         <ul className="profile-items">
-            <li>Name</li>
-            <li>Email</li>
-            <li>Gender</li>
+            Personal Information
+            <li className="list-items">Name<span>
+             {user?.name}
+            </span>
+                 </li>
+            <li className="list-items">Email Address</li>
+            <li className="list-items">Your Gender
+                <div>
+                <input type="radio" className="gender-btn"/> Male 
+                <input type="radio" className="gender-btn" /> Female
+                </div>
+            </li>
         </ul>
-        <div>
+        <div className="delete-btn">
             <span>Delete Account</span>
+        </div>
         </div>
         </div>
     )
