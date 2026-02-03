46c6cf7feefc172734a6beb49cf0af6122dc93bf
dewashishm <dewashishm97@gmail.com>
2026-02-03 23:44:26 +0530

    Style: changes in input box and its css

--- frontend/src/Pages/Profile/Profile.jsx
+++ frontend/src/Pages/Profile/Profile.jsx
@@ -1,55 +1,74 @@
 import { useEffect, useState } from "react";
 import "./Profile.css"
 
-function Profile () {
+function Profile() {
 
     const [user, setUser] = useState(null);
 
-    useEffect(()=>{
+    useEffect(() => {
 
         const storedUser1 = localStorage.getItem("user");
-        
+
         if (storedUser1) {
-            try{
+            try {
                 setUser(JSON.parse(storedUser1));
                 // console.log(storedUser1);
             }
-          catch (err){
-            console.log("Invalid user data");
-            setUser(null);
-          }
+            catch (err) {
+                console.log("Invalid user data");
+                setUser(null);
+            }
         }
-          else{
+        else {
             setUser(null);
-          }
+        }
     }, []);
 
-    
-    return(
+
+    return (
 
         <div className="profile">
-        <div className="profile-2">
-        <ul className="profile-items">
-            Personal Information
-            <li className="list-items">Name<span>
-             {user?.name}
-            </span>
-                 </li>
-            <li className="list-items">Email Address</li>
-            <li className="list-items">Your Gender
-                <div>
-                <input type="radio" className="gender-btn"/> Male 
-                <input type="radio" className="gender-btn" /> Female
+            <div className="profile-2">
+                <ul className="profile-items">
+                    Personal Information
+                    <li className="list-items">
+                        <div>
+                        Name
+                        </div>
+                        <input className="userdata-field" type="text" value={user?.name} readOnly />
+                    </li>
+
+
+                    <li className="list-items">
+                        <div>
+                            Email Address
+                        </div>
+                        <input className="userdata-field" type="text" value={user?.email} readOnly />
+                    </li>
+
+
+                    <li className="list-items">Your Gender
+                        <div className="genderdata-field">
+                            <div className="d">
+                            <input type="radio" className="gender-btn" />
+                            Male
+                            </div> 
+                        
+                            <div className="d">
+                            <input type="radio" className="gender-btn"
+                             /> 
+                                Female
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <div className="delete-btn">
+                    <span>Delete Account</span>
                 </div>
-            </li>
-        </ul>
-        <div className="delete-btn">
-            <span>Delete Account</span>
-        </div>
-        </div>
+            </div>
         </div>
     )
-    
+
 }
 
 export default Profile;
