85afc4f4e3f68d2c567e5c4e8add8a184332c2a4
dewashishm <dewashishm97@gmail.com>
2026-02-08 23:51:59 +0530

    Implement product listing with API data and specific routing

--- frontend/src/Components/Product Card/ProductCard.jsx
+++ frontend/src/Components/Product Card/ProductCard.jsx
@@ -1,27 +1,50 @@
-import { useEffect } from "react";
+import "./ProductCard.css"
+import { useEffect, useState } from "react";
 import { getProducts } from "../../api/productApi";
+import { Link, useParams } from "react-router-dom";
 
 function ProductCard() {
+  const {productid} = useParams();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    async function fetchData() {
-      const data = await getProducts(10,0);
+    async function fetchProducts() {
+      try {
+        const data = await getProducts(10, 0);
+        console.log(data);
 
-      console.log("productspage data:", data);
-      console.log("only products:", data.products);
-      
+        setProducts(data.products);
+      }
+      catch (err) {
+        console.error(err);
+      }
+      finally {
+        setLoading(false);
+      }
     }
 
-    fetchData();
-  })
+    fetchProducts();
+  }, []);
+
+  if (loading) return <p>Loading products....</p>
   return (
-    <div className="product-card">
-      <div className="image">
-      
-        
+    <div>
+      <h1>Products</h1>
+
+      <div className="products-grid">
+        {products.map((product) => (
+          <Link to={`/products/${product.id}`} key={product.id} className="product-link">
+            <div className="product-card">
+              <img src={product.thumbnail} alt={product.title} />
+              <h3>{product.brand}</h3>
+              <h3>{product.title}</h3>
+              <p>₹ {product.price}</p>
+              <p>Rating⭐: {product.rating}</p>
+            </div>
+          </Link>
+        ))}
       </div>
-      <h4>Product Name</h4>
-      <p>₹999</p>
     </div>
   );
 }
