483dbb77aebc71f0cd75d6ccf8848eb22d51d753
dewashishm <dewashishm97@gmail.com>
2026-05-09 03:52:36 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const { default: mongoose } = require("mongoose");
 
 
 dotenv.config();
@@ -17,9 +18,11 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
-app.get("*", (req, res) => {
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get((req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
@@ -29,7 +32,8 @@ app.use('/api/products', require("./routes/productRoutes"));
 app.use("/uploads", express.static("uploads"));
 app.use("/api/cart", cartRoutes);
 const PORT = process.env.PORT || 5000;
+
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    
-})
+  console.log(`Server running on port ${PORT}`);
+});
