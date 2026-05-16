7612afe757c357866f92e24a45fdf0a2ae886553
dewashishm <dewashishm97@gmail.com>
2026-05-16 23:54:51 +0530

    .

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,7 @@ const Product = require("../models/product")
 //GET all products
 exports.getProducts = async (req, res) => {
     try{
-        const products = await Product.find();
+        const products = await Product.find({});
         res.json(products);
     } catch (error) {
         res.status(500).json({message: error.message});
